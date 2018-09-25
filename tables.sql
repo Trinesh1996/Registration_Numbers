577917f587cd4ea1d1341fb1c0f0a434219945bc
Trinesh1996 <trineshchetty@gmail.com>
2018-09-25 21:55:16 +0200

    rearranged sql table

--- tables.sql
+++ tables.sql
@@ -1,16 +1,15 @@
-create table registration_numbers(
-	id serial not null primary key,
-	reg_number text not null,
-	town_id int,
-	FOREIGN key(town_id) REFERENCES cities(id)
-);
-
 create table cities(
 	id serial not null primary key,
 	town text not null,
 	regprefix text not null
 );
 
+create table registration_numbers(
+	id serial not null primary key,
+	reg_number text not null,
+	town_id int,
+	FOREIGN key(town_id) REFERENCES cities(id)
+);
 
 insert into cities(id,town,regprefix) values (1,'CapeTown','CA');
 insert into cities(id,town,regprefix) values (2,'Bellville','CY');
